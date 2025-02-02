0ce29e15529afed2d162ce028047f5a0ba9c65f4
jameskong <jameskong098@gmail.com>
2025-02-01 22:51:32 -0500

    add proper theming, fix navigation issue

--- TabMenus.swift
+++ TabMenus.swift
@@ -3,6 +3,26 @@ import SwiftUI
 struct TabMenus: View {
     @State private var selectedTab = 1
 
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        
+        appearance.backgroundColor = UIColor { traitCollection in
+            if traitCollection.userInterfaceStyle == .dark {
+                return UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 0.7)
+            } else {
+                return UIColor.white
+            }
+        }
+        
+        UITabBar.appearance().standardAppearance = appearance
+        
+        // For iOS 15 and later, update the scrollEdgeAppearance as well
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -10,20 +30,17 @@ struct TabMenus: View {
                     .font(.title)
                     .fontWeight(.bold)
                     .padding()
-
                 TabView(selection: $selectedTab) {
                     ProgressTracker()
                         .tabItem {
                             Label("Progress", systemImage: "chart.bar")
                         }
                         .tag(0)
-
                     Exercise()
                         .tabItem {
                             Label("Workout", systemImage: "figure.walk")
                         }
                         .tag(1)
-
                     Settings()
                         .tabItem {
                             Label("Settings", systemImage: "gearshape")
@@ -31,9 +48,9 @@ struct TabMenus: View {
                         .tag(2)
                 }
             }
-        }
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
-
+    
     private var currentTabName: String {
         switch selectedTab {
         case 0:
