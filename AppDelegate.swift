97a2dd16933a67f1e328a94655a9ca9dbbfa8bf6
jameskong <jameskong098@gmail.com>
2025-02-06 07:02:16 -0500

    add all orientation support, add toggle for ultra wide camera if available, make pages scrollable

--- AppDelegate.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return .portrait
-    }
-}
