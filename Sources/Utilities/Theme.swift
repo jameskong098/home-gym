4a3b54c6940c6b4e9cb85b81b1dd0d704c81ebcd
jameskong <jameskong098@gmail.com>
2025-02-10 18:30:04 -0500

    re-organize file structure

---
+++ Sources/Utilities/Theme.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct Theme {
+    static let headerColorLight = UIColor(white: 0.85, alpha: 1.0)
+    static let headerColorDark = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 0.7)
+
+    static let footerItemColorLight = UIColor(red: 70/255, green: 70/255, blue: 70/255, alpha: 1.0)
+    static let footerItemColorDark = UIColor(white: 0.85, alpha: 1.0)
+    static let footerBackgroundColorLight = UIColor(white: 0.85, alpha: 1.0)
+    static let footerBackgroundColorDark = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 0.7)
+    static let footerAccentColor = Color.blue
+
+    static let mainContentBackgroundColorLight = UIColor(white: 0.95, alpha: 1.0)
+    static let mainContentBackgroundColorDark = UIColor.black
+    
+    static let exerciseListItemIconColorLight = UIColor.white
+    static let exerciseListItemIconColorDark = UIColor.black
+    static let exerciseListItemTextColorLight = UIColor.black
+    static let exerciseListItemTextColorDark = UIColor.white
+    static let exerciseListItemBackgroundColorLight = UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 0.5)
+    static let exerciseListItemBackgroundColorDark = UIColor.white
+    static let exerciseListBackgroundColorLight = UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 0.2)
+    static let exerciseListBackgroundColorDark = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.7)
+    
+    static let settingsSectionBackgroundColorLight = UIColor.white
+    static let settingsSectionBackgroundColorDark = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.7)
+    static let settingsThemeTextColorLight = UIColor.black
+    static let settingsThemeTextColorDark = UIColor.white
+    static let toggleSwitchColor = Color.blue
+    static let settingsInfoSaveButton = Color.blue
+}
---
+++ Views/Settings.swift
@@ -0,0 +1,184 @@
+import SwiftUI
+
+struct Settings: View {
+    @AppStorage("enableTutorials") private var enableTutorials = true
+    @AppStorage("enableAutomaticTimer") private var enableAutomaticTimer = true
+    @AppStorage("useWideAngleCamera") private var useWideAngleCamera = true
+    @AppStorage("enableSoundCues") private var enableSoundCues = true
+    @AppStorage("enableVoice") private var enableVoice = true
+    @AppStorage("showBodyTrackingPoints") private var showBodyTrackingPoints = true
+    @AppStorage("showBodyTrackingLabels") private var showBodyTrackingLabels = false
+    @AppStorage("showBodyTrackingLines") private var showBodyTrackingLines = true
+    @AppStorage("themePreference") private var themePreference = "system"
+    @AppStorage("heightFeet") private var heightFeet = 5
+    @AppStorage("heightInches") private var heightInches = 7
+    @AppStorage("bodyWeight") private var bodyWeight = 70.0
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("General")
+                    .font(.headline)
+                    .padding(.top)
+
+                VStack(alignment: .center, spacing: 10) {
+                    Toggle("Show Tutorials", isOn: $enableTutorials)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: enableTutorials) { _ in
+                            triggerHapticFeedback()
+                        }
+                    Divider()
+                    Toggle("Automatic Timer", isOn: $enableAutomaticTimer)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: enableAutomaticTimer) { _ in
+                            triggerHapticFeedback()
+                        }
+                    if ARCameraViewController.deviceSupportsUltraWide() {
+                        Divider()
+                        Toggle("Use Ultra Wide Camera", isOn: $useWideAngleCamera)
+                            .padding(.horizontal)
+                            .tint(Theme.toggleSwitchColor)
+                            .onChange(of: enableTutorials) { _ in
+                                triggerHapticFeedback()
+                            }
+                    }
+                }
+                .padding()
+                .background(Color(UIColor { traitCollection in
+                    if (traitCollection.userInterfaceStyle == .dark) {
+                        return Theme.settingsSectionBackgroundColorDark
+                    } else {
+                        return Theme.settingsSectionBackgroundColorLight
+                    }
+                }))
+                .cornerRadius(25)
+
+                Text("Audio")
+                    .font(.headline)
+                    .padding(.top)
+
+                VStack(alignment: .center, spacing: 10) {
+                    Toggle("Enable Sound Cues", isOn: $enableSoundCues)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: enableSoundCues) { _ in
+                            triggerHapticFeedback()
+                        }
+                    Divider()
+                    Toggle("Enable Voice", isOn: $enableVoice)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: enableVoice) { _ in
+                            triggerHapticFeedback()
+                        }
+                }
+                .padding()
+                .background(Color(UIColor { traitCollection in
+                    if (traitCollection.userInterfaceStyle == .dark) {
+                        return Theme.settingsSectionBackgroundColorDark
+                    } else {
+                        return Theme.settingsSectionBackgroundColorLight
+                    }
+                }))
+                .cornerRadius(25)
+
+                Text("Body Tracking")
+                    .font(.headline)
+                    .padding(.top)
+
+                VStack(alignment: .center, spacing: 10) {
+                    Toggle("Show Body Tracking Points", isOn: $showBodyTrackingPoints)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: showBodyTrackingPoints) { _ in
+                            triggerHapticFeedback()
+                        }
+                    Divider()
+                    Toggle("Show Body Tracking Labels", isOn: $showBodyTrackingLabels)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: showBodyTrackingLabels) { _ in
+                            triggerHapticFeedback()
+                        }
+                    Divider()
+                    Toggle("Show Body Tracking Lines", isOn: $showBodyTrackingLines)
+                        .padding(.horizontal)
+                        .tint(Theme.toggleSwitchColor)
+                        .onChange(of: showBodyTrackingLines) { _ in
+                            triggerHapticFeedback()
+                        }
+                }
+                .padding()
+                .background(Color(UIColor { traitCollection in
+                    if (traitCollection.userInterfaceStyle == .dark) {
+                        return Theme.settingsSectionBackgroundColorDark
+                    } else {
+                        return Theme.settingsSectionBackgroundColorLight
+                    }
+                }))
+                .cornerRadius(25)
+
+                Text("Personal Information")
+                    .font(.headline)
+                    .padding(.top)
+
+                VStack(alignment: .center, spacing: 10) {
+                    HStack {
+                        Text("Height")
+                        Spacer()
+                        TextField("Feet", value: $heightFeet, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                            .padding(.horizontal)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Text("ft")
+                        TextField("Inches", value: $heightInches, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                            .padding(.horizontal)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Text("in")
+                    }
+                    Divider()
+                    HStack {
+                        Text("Body Weight (lb)")
+                        Spacer()
+                        TextField("Body Weight", value: $bodyWeight, formatter: NumberFormatter())
+                            .keyboardType(.decimalPad)
+                            .padding(.horizontal)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                }
+                .padding()
+                .background(Color(UIColor { traitCollection in
+                    if (traitCollection.userInterfaceStyle == .dark) {
+                        return Theme.settingsSectionBackgroundColorDark
+                    } else {
+                        return Theme.settingsSectionBackgroundColorLight
+                    }
+                }))
+                .cornerRadius(25)
+
+                Text("Theme")
+                    .font(.headline)
+                    .padding(.top)
+                
+                ThemeSelectionButton()
+                    .padding(.horizontal)
+                
+                Text("Selecting system will use your device's system settings.")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+
+    private func triggerHapticFeedback() {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+    }
+}
